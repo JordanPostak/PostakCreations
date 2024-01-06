a914dc9bc36935b9928429117d3770bc88ab61b0
JordanPostak <jordan_postak@hotmail.com>
2024-01-06 12:42:44 -0700

    hopefully fixed

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,20 +1,20 @@
+var dropdownVisible = false; // Variable to track the current state
+
 document.getElementById('dropdown-toggle').addEventListener('click', function () {
     var dropdown = document.getElementById('dropdown');
     var arrow = document.querySelector('#dropdown-toggle .arrow');
 
-    if (dropdown.classList.contains('hidden')) {
-        dropdown.classList.remove('hidden');
-        dropdown.classList.add('unhidden');
-        dropdown.style.height = dropdown.scrollHeight + 'px'; // Set height to the actual height
+    if (!dropdownVisible) {
+        // Dropdown is currently hidden
+        dropdown.style.height = dropdown.scrollHeight + 'px';
         arrow.classList.add('rotate180');
     } else {
-        dropdown.classList.remove('unhidden');
-        dropdown.classList.add('hidden');
-
-        // Set different initial height based on screen width
+        // Dropdown is currently visible
         var initialHeight = window.innerWidth <= 767 ? '100px' : '125px';
-
         dropdown.style.height = initialHeight;
         arrow.classList.remove('rotate180');
     }
+
+    // Toggle the state for the next click
+    dropdownVisible = !dropdownVisible;
 });
